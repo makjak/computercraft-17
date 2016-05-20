8ae5a31b5a01c2d7274944d54fba416c1b29f5f0
Niels Dommerholt <niels.dommerholt@jdriven.com>
2016-05-20 11:43:39 +0200

    New reactor logger

--- install-data.lua
+++ install-data.lua
@@ -5,6 +5,7 @@
 -- { app = "base-energy", deps = {"hydraApi"}, id="xQj3bUCp", saveas="startup"  },
 -- { app = "base-bees", deps = {"hydraApi"}, id="qe0ViX8i", saveas="startup"  },
 -- { app = "base-ae-dump", deps = {"hydraApi"}, id="hUsQFbpi", saveas="aed"  },
-{ app = "base-reactor", deps = {"hydraApi"}, id="reactor.lua", saveas="rea"  },
-{ app = "hydraApi", deps = nil, id="hydraapi.lua", saveas=nil, api=true }
+    { app = "base-reactor", deps = {"hydraApi"}, id="reactor.lua", saveas="rea"  },
+    { app = "log-reactor", deps = {"hydraApi"}, id="log-reactor.lua", saveas="logr"  },
+    { app = "hydraApi", deps = nil, id="hydraapi.lua", saveas=nil, api=true }
 }
