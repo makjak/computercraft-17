c23caa1ad3ca10e34be3ec31f395fd70e6678e74
Niels Dommerholt <niels.dommerholt@jdriven.com>
2016-05-21 13:47:17 +0200

    Timers update

--- timers.lua
+++ timers.lua
@@ -2,7 +2,42 @@ if(not os.loadAPI("hydraApi")) then
     error("Could not load hydraApi")
 end
 
-local config = hydraApi.loadConfig("timers.cfg")
+local timers = {}
+local config
+local monitor = hydraApi.getMonitor()
+
+function tick()
+    -- redstone.setBundledOutput("left", 1)
+end
+
+function createTimers(config)
+    for k,v in pairs(config) do
+        timers[k] = {}
+        timers[k]["state"] = 0
+        timers[k]["count"] = 0
+    end
+end
+
+function writeTimers()
+    if(monitor == nil) then
+        return
+    end
+    monitor.clear()
+    local row = 1
+    for k,v in pairs(reactorIds) do
+        monitor.setCursorPos(1, row)
+        monitor.write()
+        row = row + 1
+    end
+end
+
+function formatForScreen(k)
+    local value = tostring(k) .. ": " .. tostring(config[k]["high"])
+
+    return value
+end
+
+config = hydraApi.loadConfig("timers.cfg")
 
 if(config == nil) then
     config = {}
@@ -14,5 +49,12 @@ if(config == nil) then
 
     hydraApi.saveConfig(config, "timers.cfg")
 end
+
+while true do
+    tick()
+    writeTimers()
+    sleep(1)
+end
+
 
 
