d44cc9425c9467d2ef4ec8ba4aa7d16d94e8b20c
Niels Dommerholt <niels.dommerholt@jdriven.com>
2016-05-29 10:59:01 +0200

    Added config

--- energy.lua
+++ energy.lua
@@ -11,16 +11,36 @@ monitor.clear()
 
 local capacitors = hydraApi.getAllPeripherals('tile_blockcapacitorbank_name')
 
+local config
+
 function displayList()
     local row = 2
     monitor.setCursorPos(1,1)
-    monitor.write("RF     %")
+    monitor.write("RF         %")
     for key, c in pairs(capacitors) do
+        local stored = c.getEnergyStored() * config[key]['size']
         monitor.setCursorPos(1,row)
-        monitor.write(hydraApi.padLeft(hydraApi.formatEnergy(c.getEnergyStored()), 8) .. ' ')
+        monitor.write(hydraApi.padLeft(hydraApi.formatEnergy(stored), 10) .. ' ')
         row = row + 1
     end
 end
+
+function loadConfig(capacitors)
+    config = hydraApi.loadConfig("energy.cfg")
+
+    if(config == nil) then
+        config = {}
+
+        for key, c in pairs(capacitors) do
+            config[key] = {}
+            config[key]['size'] = 1
+        end
+
+        hydraApi.saveConfig(config, "energy.cfg")
+    end
+end
+
+loadConfig(capacitors)
 
 while true do
     displayList()
